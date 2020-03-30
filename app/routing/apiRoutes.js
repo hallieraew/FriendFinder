7788eb5530891c59a62f80fee3cca12292c354c2
hallieraew <hallieweintraub@gmail.com>
2020-03-29 22:10:49 -0500

    added in button click for survey and port number, cannot get style to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,42 +1,43 @@
 var friends = require('../data/friends');
-require('../../server');
 
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        return res.json(friends);
+    });
 
-app.get("/api/friends", function (req, res) {
-    return res.json(friends);
-});
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+        console.log(newFriend);
 
-app.post("/api/friends", function (req, res) {
-    var newFriend = req.body;
-    console.log(newFriend);
-
-    for (var i = 0; i<friends.scores.length; i++){
-        user.scores[i] = parseInt(user.scores[i]);
-    }
-
-    
-    for(var i = 0; i < friends.length; i++) {
-        var bestMatch = 0;
-        var highestDiff = 50;
-        var totalDifference = 0;
-        for(var j = 0; j < friends[i].scores.length; j++) {
-            var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
-            totalDifference += difference;
+        for (var i = 0; i < friends.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
         }
-        
-        if(totalDifference < highestDiff) {
-            bestMatch = bestMatchIndex;
-            bestMatchIndex= i;
-            highestDiff = totalDifference;
+
+
+        for (var i = 0; i < friends.length; i++) {
+            var bestMatch = 0;
+            var highestDiff = 50;
+            var totalDifference = 0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+                totalDifference += difference;
+            }
+
+            if (totalDifference < highestDiff) {
+                bestMatch = bestMatchIndex;
+                bestMatchIndex = i;
+                highestDiff = totalDifference;
+            }
         }
-      }
 
-      friends.push(user);
+        friends.push(user);
 
-      res.json(friends[bestMatchIndex]);
-    
-    // create logic here for compatability between new friend and array of friends
-    // need to loop through friends.length and determine scores and then for every new user score compute difference between new user and friends i
+        res.json(friends[bestMatchIndex]);
 
-    return req.json(friends[bestMatchIndex]);
-});
+        // create logic here for compatability between new friend and array of friends
+        // need to loop through friends.length and determine scores and then for every new user score compute difference between new user and friends i
+
+        return req.json(friends[bestMatchIndex]);
+    });
+
+};
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require("express");
 
 var app = express();
-var PORT = 3000;
+var PORT = 8000;
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App is listening on PORT " + PORT);
 });
 
-module.exports = ('server.js');
+module.exports = app;
