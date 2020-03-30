4e4e22cfbf64266114264c798f88bfc866f73f69
hallieraew <hallieweintraub@gmail.com>
2020-03-30 14:16:06 -0500

    adding in code for static style sheet

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express");
+var path = require("path")
 
 var app = express();
 var PORT = 8000;
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
