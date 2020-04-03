a7764afec7f45aada6f6610759ff9fe86e08c09e
hallieraew <hallieweintraub@gmail.com>
2020-04-03 13:44:24 -0500

    adding in comments to the apiRoute js to explain logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,27 +6,36 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
+        // setting variable for the survey responses
         var newFriend = req.body;
         console.log(newFriend);
 
+        // setting a bestMatch to start at 0 so we can add diff for each user comparison
         var bestMatch = 0;
+        // setting a lowestDiff above the highest variance possible so every user with be a match, determining what the lowest diff will be below
         var lowestDiff = 50;
 
+        // looping through the friends array
         for (var i = 0; i < friends.length; i++) {
             var totalDifference = 0;
+            // looping through the scores for each friend
             for (var j = 0; j < friends[i].scores.length; j++) {
+                // finding the difference by comparing scores of the new friend against current friends using math.abs so the number is absolute and not negative
                 var difference = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+                // adding the difference found to the totalDifference
                 totalDifference += difference;
             }
-
+            // determining if totalDifference is less than the lowestDiff set above
             if (totalDifference < lowestDiff) {
+                // setting bestMatch as index of friend
                 bestMatch = i;
+                // lowestDiff redefined to the totalDiff
                 lowestDiff = totalDifference;
             }
         }
-
+        // push newFriend into the friends array after doing the comparison
         friends.push(newFriend);
-
+        // res in json is friends with bestmatch determined index
         res.json(friends[bestMatch]);
 
     });
