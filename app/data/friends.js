3320def204b557d9b9acac87e1a6a038859f30b1
hallieraew <hallieweintraub@gmail.com>
2020-04-04 10:18:55 -0500

    placeholders

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,31 @@
-var friends = [
-    {
+var friends = [{
         name: "Marge",
-        image: "",
+        image: "https://via.placeholder.com/150",
         scores: [4, 2, 1, 3, 2, 5, 5, 1, 2, 5]
     },
     {
         name: "Bonnie",
-        image: "",
+        image: "https://via.placeholder.com/150",
         scores: [3, 4, 3, 5, 5, 1, 3, 1, 2, 1]
     },
     {
         name: "Andre",
-        image: "",
+        image: "https://via.placeholder.com/150",
         scores: [3, 5, 2, 3, 3, 5, 4, 2, 2, 1]
     },
     {
         name: "Naida",
-        image: "",
+        image: "https://via.placeholder.com/150",
         scores: [3, 2, 3, 5, 5, 3, 2, 2, 4, 3]
     },
     {
         name: "Nestor",
-        image: "",
+        image: "https://via.placeholder.com/150",
         scores: [3, 4, 1, 2, 5, 4, 3, 4, 4, 5]
     },
     {
         name: "Mariella",
-        image: "",
+        image: "https://via.placeholder.com/150",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     }
 ];
